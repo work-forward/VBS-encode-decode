45648753b6db772b16fcf9b98c635c63037e3ce3
ying2025 <zhrj2030@gmail.com>
2018-07-29 12:00:30 +0800

    调整

--- float.js
+++ float.js
@@ -10,14 +10,14 @@ function FloatOperate() {
         let [s, e, m] = _floatToNumber(v);
         let negative = s ? true : false;
         for (let i=0; i< 52; i++) {
-                if (m % 1 ==0) {  
-                    break;
-                } else {
-                    m = m * 2;
-                    e--;
-                }
-         }
-        expo = e;
+	            if (m % 1 ==0) {  
+	                break;
+	            } else {
+	                m = m * 2;
+	                e--;
+	            }
+	     }
+    	expo = e;
         if (negative) {
             mantissa = -m;
         } else {
@@ -32,13 +32,13 @@ function FloatOperate() {
     function _floatToNumber(flt)
     {
         if (isNaN(flt)) // Special case: NaN
-            return _assembleFloat(0, flt_NAN, 0); // Mantissa is nonzero for NaN
+        	return _assembleFloat(0, flt_NAN, 0); // Mantissa is nonzero for NaN
         var sign = (flt < 0) ? 1 : 0;
         flt = Math.abs(flt); 
         var exponent = Math.floor(Math.log(flt) / Math.LN2);
         // 此处微调，原先：exponent > 127 | exponent < -126
         if (exponent > 1023 | exponent < -1022) // Special case: +-Infinity (and huge numbers)
-            return _assembleFloat(sign, flt_INF, 0); // Mantissa is zero for +-Infinity
+        	return _assembleFloat(sign, flt_INF, 0); // Mantissa is zero for +-Infinity
         var mantissa = flt / Math.pow(2, exponent);
         return [sign, exponent, mantissa];
     }
@@ -47,33 +47,33 @@ function FloatOperate() {
         // console.log(mantissa, expo)
         let num, negative = false;
         if (mantissa == 0) {
-            if (expo < 0) {
-                expo = -expo;
-                negative = true;
-            } 
+        	if (expo < 0) {
+        		expo = -expo;
+        		negative = true;
+        	} 
             if (expo < flt_ZERO) {
                 num = 0;
-            } else if (expo == flt_INF) {
+        	} else if (expo == flt_INF) {
                 if (negative) {
                     num = Number.NEGATIVE_INFINITY;
                 } else {
                     num = Number.POSITIVE_INFINITY;
                 }
             } else {
-                num = Number.NaN;
-            }
+        		num = Number.NaN;
+        	}
         } else {
-            if (mantissa < 0) {
-                mantissa = -mantissa;
-                negative = true;
-            }
-            if (expo > 1023 | expo < -1022) { // Special case: +-Infinity
-               if (negative) {
-                 num = Number.NEGATIVE_INFINITY;
-               } else {
-                 num = Number.POSITIVE_INFINITY;
-               }
-            } else {
+        	if (mantissa < 0) {
+        		mantissa = -mantissa;
+        		negative = true;
+        	}
+        	if (expo >= 0x7FF) { // Special case: +-Infinity
+        	   if (negative) {
+        	   	 num = Number.NEGATIVE_INFINITY;
+        	   } else {
+        	   	 num = Number.POSITIVE_INFINITY;
+        	   }
+        	} else {
                 for (let i=0; i< 52; i++) {
                     if (mantissa < 2 && ((mantissa % 1 != 0) | mantissa == 1)) {  
                         break;
@@ -83,6 +83,7 @@ function FloatOperate() {
                     }
                 }
                 num = mantissa * Math.pow(2, expo);
+
                 if (negative) {
                     num = -num;
                 }
@@ -96,7 +97,7 @@ function breakFloat(v) {
 }
 
 function makeFloat(mantissa, expo) {
-    return new FloatOperate()._makeFloat(mantissa, expo);
+	return new FloatOperate()._makeFloat(mantissa, expo);
 }
 
 module.exports = {
